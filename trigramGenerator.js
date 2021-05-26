101998c52ce0e4ab2f04e05ff00b496545d7d3c2
VjekoslavLalic <VjekoslavLalic@users.noreply.github.com>
2021-05-26 13:45:45 +0200

    trigramGenerator

--- trigramGenerator.js
+++ trigramGenerator.js
@@ -1,11 +1,43 @@
 function trigramGenerator(text) {
     if (typeof text !== 'string') throw new Error();
 
-    const words = text.split(' ');
+    const words = whitespaceHandler(text).split(' ');
 
-    if (words.length < 3) return new Map();
+    let chunks = generateChunks(lowerCaseSveOsimI(words));
 
-    return new Map([[words.slice(0, 2).join(' '), [words[2]]]]);
+    return generateTrigrams(chunks);
 }
 
-module.exports = trigramGenerator;
+function lowerCaseSveOsimI(wordArray) {
+    return wordArray.map((word) => word != 'I' ? word.toLowerCase() : word);
+}
+
+function whitespaceHandler(text) {
+    return text.replace(/\s+/g, ' ').trim();
+}
+
+function generateChunks(words) {
+    let array = words.slice(0, words.length - 2);
+    let result = [];
+
+    array.forEach((_word, i) => {
+        result.push(words.slice(i, i + 3));
+    });
+
+    return result;
+}
+
+function generateTrigrams(words) {
+    return words.reduce((acc, value) => {
+        let key = value[0] + ' ' + value[1];
+        if (!acc.has(key)) acc.set(key, [])
+        acc.get(key).push(value[2]);
+
+        return acc;
+    }, new Map());
+}
+
+
+
+
+module.exports = { trigramGenerator, generateChunks, whitespaceHandler };
